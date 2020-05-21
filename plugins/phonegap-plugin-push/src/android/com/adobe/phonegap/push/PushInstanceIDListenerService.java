66405565c6cd62186e41a843cff6a53e78b097c0
pivotmobileapp <jupiter@pivotmobileapp.com>
2020-05-21 09:10:31 -0400

    v2s

---
+++ plugins/phonegap-plugin-push/src/android/com/adobe/phonegap/push/PushInstanceIDListenerService.java
@@ -0,0 +1,26 @@
+package com.adobe.phonegap.push;
+
+import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.firebasemessagingservice;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class PushInstanceIDListenerService extends FirebaseMessagingService implements PushConstants {
+    public static final String LOG_TAG = "Push_InsIdService";
+
+    @Override
+    public void onNewToken() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(LOG_TAG, "Refreshed token: " + refreshedToken);
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
